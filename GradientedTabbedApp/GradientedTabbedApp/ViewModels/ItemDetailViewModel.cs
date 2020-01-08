f12c9d4ad382e54414b75ac6e617740b6878bfd2
Bohdan Benetskyi (PGS Software) <bbenetskyi@pgs-soft.com>
2020-01-08 15:04:34 +0100

    Add project files.

---
+++ GradientedTabbedApp/GradientedTabbedApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GradientedTabbedApp.Models;
+
+namespace GradientedTabbedApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
